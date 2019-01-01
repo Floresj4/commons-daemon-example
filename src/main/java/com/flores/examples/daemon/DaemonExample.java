1ec1d8517033c171396fa1a26f6dd98c46fc99ed
Jason <floresj4@gmail.com>
2019-01-01 17:13:20 -0500

    adding logging sample via start and stop methods for commons

--- src/main/java/com/flores/examples/daemon/DaemonExample.java
+++ src/main/java/com/flores/examples/daemon/DaemonExample.java
@@ -1,5 +1,7 @@
 package com.flores.examples.daemon;
 
+import java.util.UUID;
+
 import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,13 +14,38 @@ public class DaemonExample {
 	
 	private static final Logger logger = LoggerFactory.getLogger(DaemonExample.class);
 	
+	static boolean isLogging = false;
+	
 	public static void main(String[] args) {
-		PropertyConfigurator.configure("log4j.properties");
-		logSomething();
+		PropertyConfigurator.configure("./src/extresources/log4j.properties");
+		
+//		logSomething();
+		start(args);
 	}
 	
-	static void logSomething() {
-		logger.debug("Logging a debug message.");
-		logger.warn("logging a warn message");
+	static void logSomething(UUID guid) {
+		logger.debug(String.format("Logging a debug event for user %s", 
+				guid.toString()));
+	}
+	
+	static void start(String args[]) {
+		while(isLogging = true) {
+			UUID guid = UUID.randomUUID();
+			logSomething(guid);
+
+			try {
+				//sleep for between 1 and 10 seconds
+				double sleepDuration = (int) (Math.random() * 10000);
+				logger.trace(String.format("Sleeping for %4.3f seconds", (sleepDuration / 1000)));
+				Thread.sleep((int)sleepDuration);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	static void stop(String args[]) {
+		isLogging = false;
+		System.exit(0);
 	}
 }
